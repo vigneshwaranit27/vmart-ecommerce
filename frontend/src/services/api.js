2874e063a8795fadba484e725f1022fb5c18a308
vignesh <vigneshwaran.it27@gmail.com>
2026-06-18 04:36:12 -0700

    fix:  api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://vmart-backend-fyed.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://vmart-backend-fyed.onrender.com/api/',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' }
 });
